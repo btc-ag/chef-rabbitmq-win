338a7158c0a93f377952321fc03675c7860b3a77
josarche <jose.sarchese@btc-ag.com>
2021-06-17 08:53:24 +0000

    bessere Syntax

--- resources/erlang.rb
+++ resources/erlang.rb
@@ -26,7 +26,7 @@ action :install do
   end
 
   if new_resource.overrideErlangHome.nil?
-    if (major >= 22) && (major < 23)
+    if (major == 22)
       pfad = "C:\\Program Files\\erl#{erl_major}"
     elsif major >= 23
       pfad = "C:\\Program Files\\erl-#{new_resource.version}"
